a9897677461238a3add6c61509e8bdc27b43a72c
Krithika <113484065+ASA77777@users.noreply.github.com>
2024-09-18 07:40:30 +0000

    Objects, bool and type function

---
+++ 02-Welcome-to-Python/the-print-function-I-single-argument.py
@@ -0,0 +1,13 @@
+print("I love sandwiches")
+print('I also love juice beverages')
+print("We're working with Python")
+
+print(5)
+print(3+4)
+print(10 - 8)
+
+print("3 + 4")
+
+print("3" + "4")
+length = len(" ")
+print(length)
---
+++ 03-Numbers-Booleans-and-Equality/type-conversion-with-the-int-float-and-str-functions.py
@@ -0,0 +1,21 @@
+print(int(3.14))
+print(int(3.99))
+print(int("3"))
+print(int(3))
+
+print(float(5))
+print(float("10.32"))
+print(float(5.23))
+
+print(str(5.35))
+print(str(5))
+print(str("Hello"))
+
+#print(5 + "Hello") #Wrong - cant add str to a number
+print(str(5) + "Hello")
+
+print(3+3)
+print(4.3+5.7)
+print(3+5.8)
+
+print(int(float("3.14")))
