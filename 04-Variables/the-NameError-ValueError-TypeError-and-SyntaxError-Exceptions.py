b64f5995359a36389effb59f271019146932b249
Krithika <113484065+ASA77777@users.noreply.github.com>
2024-09-26 00:46:33 +0000

    Types of error - basic

---
+++ 04-Variables/the-NameError-ValueError-TypeError-and-SyntaxError-Exceptions.py
@@ -0,0 +1,16 @@
+#NAME ERROR
+#first_name = "Boris"
+
+#50 lines down
+
+#print(firstname)
+
+#TYPE ERROR
+#print("5" + 3)
+#print(3 + "5")
+
+#VALUE ERROR (Right type but iincorrect value)
+#int("xyz")
+
+#SYNTAX ERROR
+#print("hello)
