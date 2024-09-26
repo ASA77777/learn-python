b3759fba1e9d1995b245a464a5212aaea8310fef
Krithika <113484065+ASA77777@users.noreply.github.com>
2024-09-26 00:47:06 +0000

    input function

---
+++ 04-Variables/user-input-with-the-input-function.py
@@ -0,0 +1,7 @@
+first_name = input("What's your first name? ")
+print("It's nice to meet you,", first_name)
+
+first_number = input("What is the first number? ")
+second_number = input("What is the second number? ")
+print("The sum of both numbers: ", int(first_number) + int(second_number))
+
