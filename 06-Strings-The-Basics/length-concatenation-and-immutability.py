d243c06b32a668965de61e2d3101127873367a0a
Krithika <113484065+ASA77777@users.noreply.github.com>
2024-10-01 00:58:54 +0000

    len

---
+++ 06-Strings-The-Basics/length-concatenation-and-immutability.py
@@ -0,0 +1,18 @@
+print(len("Python"))
+print(len("Programming"))
+print(len("@#$^^"))
+print(len(" "))
+print(len(""))
+
+#print(len(4)) #object of type int does not have any length
+#print(len(3.14)) #same with a float
+
+print("Boris" + "Paskhaver")
+print("Boris " + "Paskhaver")
+print("Boris" + " Paskhaver")
+print("Boris" + " " + "Paskhaver")
+
+print("a" "b" "c")
+
+print("---" * 30)
+
