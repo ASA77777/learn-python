c29630b1a1a5e0f6ef2666b659ab546347dc3a9d
Krithika <113484065+ASA77777@users.noreply.github.com>
2024-10-23 23:48:51 +0000

    control flow

---
+++ 08-Control-Flow/the-else-statement.py
@@ -0,0 +1,10 @@
+if 20 > 15:
+    print("That is true!")
+else:
+    print("That is false!")
+
+value = int(input("Enter a random number: "))
+if value > 100000:
+    print("nnnnn")
+else:
+    print("kkkk")
